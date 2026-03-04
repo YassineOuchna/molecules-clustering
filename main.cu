31a5ee2a2cd3cf7015bea094ef7546c6369be07c
Florian <florian.de.boni@gmail.com>
2026-03-04 16:45:37 +0100

    Last tests

--- main.cu
+++ main.cu
@@ -39,7 +39,7 @@ int main(int argc, char** args) {
 
     FileUtils file(file_name); 
 
-    size_t N_frames = 50000;
+    size_t N_frames = 99000;
     size_t N_atoms  = file.getN_atoms();
     size_t N_dims   = file.getN_dims();
 
@@ -48,7 +48,7 @@ int main(int argc, char** args) {
     file.readSnapshotsFastInPlace(0, N_frames - 1, all_data);
     std::cout << "Loaded " << N_frames * N_atoms * N_dims * sizeof(float) / (1024*1024) << " MiB into CPU RAM." << std::endl;
 
-    const size_t MAX_DATA_CHUNK_SIZE = 20000; // In MB
+    const size_t MAX_DATA_CHUNK_SIZE = 23000; // In MB
     const size_t NB_FRAMES_PER_CHUNK = get_chunk_frame_nb(MAX_DATA_CHUNK_SIZE, N_atoms, N_dims);
     const size_t NB_ROW_ITERATIONS = (size_t) std::ceil((double)N_frames / NB_FRAMES_PER_CHUNK);
     const size_t RMSD_LOOPS_NEEDED = NB_ROW_ITERATIONS * (NB_ROW_ITERATIONS + 1) / 2;
