83731b826e7706db64992e02132ae6810b658e94
Florian <florian.de.boni@gmail.com>
2026-04-01 08:36:37 +0000

    Stashing

--- md_reader.cpp
+++ md_reader.cpp
@@ -138,8 +138,8 @@ int main()
 {
     try
     {
-        std::string dataset_root = "./datasets/dataset3";
-        std::string output_file = "output/snapshots_coords_all_3.bin";
+        std::string dataset_root = "./datasets/dataset2";
+        std::string output_file = "output/snapshots_coords_all_2.bin";
 
         fs::create_directories(fs::path(output_file).parent_path());
 
@@ -173,8 +173,8 @@ int main()
 
         for (auto &subdir : subdirs)
         {
-            fs::path pdb_file = subdir / "minimal.pdb";
-            fs::path xtc_file = subdir / "minimal.xtc";
+            fs::path pdb_file = subdir / "full.pdb";
+            fs::path xtc_file = subdir / "full.xtc";
 
             if (!fs::exists(pdb_file) || !fs::exists(xtc_file))
             {
